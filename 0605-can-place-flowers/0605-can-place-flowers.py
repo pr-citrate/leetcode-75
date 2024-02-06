bc18900c74d6f3ade77a11d7784ebc202788f305
pr-citrate <123797964+pr-citrate@users.noreply.github.com>
2024-02-07 01:43:50 +0900

    Time: 136 ms (56.94%), Space: 16.9 MB (77.04%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -4,27 +4,21 @@ class Solution:
             return n + flowerbed[0] != 2
 
         cnt: int = 0
-        last_idx: int = -1
         idx: int = 2
+        
         if flowerbed[0] + flowerbed[1] == 0:
             cnt += 1
             last_idx = 0
             idx += 1
         
         while idx < len(flowerbed):
-            print(idx)
             if flowerbed[idx] + flowerbed[idx - 1] + flowerbed[idx - 2] == 0:
                 cnt += 1
                 idx += 2
-                last_idx: idx
             else:
                 idx += 1
 
-        print(idx == len(flowerbed), last_idx < len(flowerbed) - 2, flowerbed[-1] + flowerbed[-2] == 0)
-
-        if idx == len(flowerbed) and last_idx < len(flowerbed) - 2 and flowerbed[-1] + flowerbed[-2] == 0:
-            print(idx)
+        if idx == len(flowerbed) and 3 <= len(flowerbed) and flowerbed[-1] + flowerbed[-2] == 0:
             cnt += 1
-        
-        print(cnt)
+
         return cnt >= n
